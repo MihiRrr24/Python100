a4e5a1161e5cf46a51eb08f6675a2908ebedd166
Mihir Kumar Shrestha <121042506+Mihirr42@users.noreply.github.com>
2024-03-27 09:21:47 +0530

    SingleLineComments

---
+++ Day 5/singleLine.py
@@ -0,0 +1,7 @@
+#  This IS A POEM
+#  I have ASsigned a task
+
+print("---The woods are lovely, dark and deep,")
+print("But I have promises to keep,")
+print("And miles to go before I sleep,")
+print("And miles to go before I sleep.---")         # POEM COMPLETE
