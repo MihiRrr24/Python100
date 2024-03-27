c2298d7fb1f4ca7b69a8fce101db584cf8b58e2e
Mihir Kumar Shrestha <121042506+Mihirr42@users.noreply.github.com>
2024-03-27 08:26:32 +0530

    Poem

---
+++ Day 4/poem.py
@@ -0,0 +1,6 @@
+# Write a program to print a poem in Python. Choose the poem of your choice
+
+print("---The woods are lovely, dark and deep,")
+print("But I have promises to keep,")
+print("And miles to go before I sleep,")
+print("And miles to go before I sleep.---")
