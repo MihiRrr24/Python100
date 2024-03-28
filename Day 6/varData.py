a8c90d082cbbff1e5b17937304843e7d2deaaabc
Mihir Kumar Shrestha <121042506+Mihirr42@users.noreply.github.com>
2024-03-28 08:42:06 +0530

    Variables and Data Types

---
+++ Day 6/varData.py
@@ -0,0 +1,16 @@
+a = complex(8, 2)     # Complex data type
+b = True                # Boolean
+c = "Harry"               # String
+d = None                  # NoneType
+print(a)
+print(b)
+a1 = 9
+print(a + a1)               # Addition of two data Types variables
+
+
+
+# Printing the type of variable
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print("The type of c is ", type(d))
