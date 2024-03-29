fb9f3f9a8a6f26006e3260ec32e792ece6dc3847
Mihir Kumar Shrestha <121042506+Mihirr42@users.noreply.github.com>
2024-03-29 17:53:10 +0530

    Create Calculator.py

---
+++ Day 8/Calculator.py
@@ -0,0 +1,10 @@
+num1 = 5
+num2 = 2
+
+print("Addition of two numbers: ",num1+num2)
+print("Subtraction of two numbers: ",num1-num2)
+print("Multiplication of two numbers: ",num1*num2)
+print("Division of two numbers: ",num1/num2)
+print("Floor Division of two numbers: ",num1//num2)
+print("Modulus of two numbers: ",num1%num2)
+print("Exponential of two numbers: ",num1**num2)
